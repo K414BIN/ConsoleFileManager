bffecde68a138e681d546ab2ba31e6d474e3f505
Alexander Kalabin <k414bin@yandex.ru>
2021-03-28 10:00:06 +0300

    Class DirTree added

---
+++ DirTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleFileManager
+{
+    class DirTree
+    {
+        public static void filesTree(string workDir)
+        {
+            string fileTree = "DIRtree.txt";
+            if (Directory.Exists(workDir))
+            {
+                string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.AllDirectories);
+                for (int t = 0; t < entries.Length; t++)
+                {
+                    File.AppendAllText(fileTree, Environment.NewLine);
+                    File.AppendAllText(fileTree, entries[t]);
+                }
+            }
+        }
+    }
+}
